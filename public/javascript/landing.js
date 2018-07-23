0b4a2bebf10fd7153cf9f61657d0caa8362b15ab
PraveenKumarRana <ced17i019@iiitdm.ac.in>
2018-07-23 11:33:51 +0530

    Added dropdown element and JS is remaining

---
+++ public/javascript/landing.js
@@ -0,0 +1,7 @@
+// HEADER LOGIC
+// ===============================================================
+
+var education = document.getElementById("education");
+education.addEventListener("click", function(){
+    education.classList.toggle("hidden");
+});
